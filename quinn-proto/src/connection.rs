4f0f13feb1ac9aae5064dac48a4ea8a9718008f0
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-12-12 17:27:15 +0100

    Add comment about unused From impl

--- quinn-proto/src/connection.rs
+++ quinn-proto/src/connection.rs
@@ -2359,6 +2359,7 @@ impl From<TransportError> for ConnectionError {
     }
 }
 
+// For compatibility with API consumers
 impl From<ConnectionError> for io::Error {
     fn from(x: ConnectionError) -> io::Error {
         use self::ConnectionError::*;
